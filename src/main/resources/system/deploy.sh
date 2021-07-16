ec88c4ecb97c50e16d6886ef32aae4fe113f5132
anpopov <anpopov@tectus-it.ru>
2021-07-16 18:35:33 +0300

    GNSS-4: fix deployment script

--- src/main/resources/system/deploy.sh
+++ src/main/resources/system/deploy.sh
@@ -1,5 +1,5 @@
-scp -C -i C:\\Users\\andre\\.ssh\\id_rsa_1 -r C:\\Work\\BMSTU\\GNSS\\lab-gnss\\target\\lab-gnss-0.0.1-SNAPSHOT.jar anpopov@lab-gnss.bmstu.ru:/home/anpopov
+scp -C -i C:\\Users\\andre\\.ssh\\id_rsa_1 C:\\Work\\BMSTU\\GNSS\\lab-gnss\\target\\lab-gnss-0.0.1-SNAPSHOT.jar anpopov@lab-gnss.bmstu.ru:/home/anpopov/lab-gnss-0.0.1-SNAPSHOT.jar
 
 ssh lab-gnss "sudo systemctl stop lab-gnss-app;
 sudo mv /home/anpopov/lab-gnss-0.0.1-SNAPSHOT.jar /opt/lab-gnss/back/;
-sudo systemctl start lab-gnss-app"
+sudo systemctl start lab-gnss-app;"
